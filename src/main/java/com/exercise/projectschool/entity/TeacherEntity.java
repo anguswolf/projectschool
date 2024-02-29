650f0e9e856763f5512801398a51afbcbb1ccf41
Augusto C <augusto.ciuccatosti@gmail.com>
2024-03-01 00:52:51 +0100

    First commit

---
+++ src/main/java/com/exercise/projectschool/entity/TeacherEntity.java
@@ -0,0 +1,28 @@
+package com.exercise.projectschool.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity(name = "TeacherEntity")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "teacher")
+public class TeacherEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    Long id;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "email")
+    String email;
+
+    @Column(name = "matter")
+    String matter;
+}
---
+++ src/main/java/com/exercise/projectschool/model/Student.java
@@ -0,0 +1,18 @@
+package com.exercise.projectschool.model;
+
+import io.swagger.annotations.ApiModel;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@ApiModel(description = "Student")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Student {
+    String name;
+    String city;
+    String age;
+    String school;
+}
