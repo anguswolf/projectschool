c1f5624b93fd92193255fdf01232cdce63c3c004
Augusto C <augusto.ciuccatosti@gmail.com>
2024-03-05 18:05:36 +0100

    Implement Logic Business Course Controller

--- src/main/java/com/exercise/projectschool/service/CourseService.java
+++ src/main/java/com/exercise/projectschool/service/CourseService.java
@@ -1,10 +1,13 @@
 package com.exercise.projectschool.service;
 
 import com.exercise.projectschool.entity.StudentEntity;
+import com.exercise.projectschool.entity.TeacherEntity;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface CourseService {
     ResponseEntity<List<StudentEntity>> listStudentByTeacher(String serialNumber);
+
+    ResponseEntity<Void> addStudentToCourse(StudentEntity student,String serialNumber);
 }
