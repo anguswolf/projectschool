01bf3dbce9b213e0055059b91a1c71d3a4988e38
Augusto C <augusto.ciuccatosti@gmail.com>
2024-03-22 11:43:57 +0100

    Implement Description With OpenAPI Swagger

--- src/main/java/com/exercise/projectschool/controller/StudentController.java
+++ src/main/java/com/exercise/projectschool/controller/StudentController.java
@@ -4,6 +4,13 @@ import com.exercise.projectschool.dto.StudentDTO;
 import com.exercise.projectschool.entity.StudentEntity;
 import com.exercise.projectschool.model.Student;
 import com.exercise.projectschool.service.StudentService;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +20,64 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path= "/student")
+@Tag(name = "Student Management")
 public class StudentController {
     private final StudentService studentService;
 
+
     @GetMapping(path = "/{serialNumber}")
+    @Hidden
     public ResponseEntity<List<StudentEntity>> getStudent(@PathVariable String serialNumber) {
         return studentService.getStudentBySerialNumber(serialNumber);
     }
 
+    @Operation(
+            description = "Get Student by SerialNumber with JPA",
+            summary = "Return List of Student by SerialNumber with JPA",
+            responses = {
+                    @ApiResponse(
+                            description = "Find student by SerialNumber: (SerialNumber)",
+                            responseCode = "200",
+                            content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StudentDTO.class),
+                            examples = @ExampleObject(value = """
+                                        {
+                                          "id": 1,
+                                          "name": "John Doe",
+                                          "city": "New York",
+                                          "capital": "Washington D.C.",
+                                          "age": "25",
+                                          "school": "XYZ High School",
+                                          "serialNumber": "S123",
+                                          "classRoom": "2A",
+                                          "teachers": [
+                                            {
+                                              "id": 1,
+                                              "name": "Jane Smith",
+                                              "email": "jane@example.com",
+                                              "matter": "Math",
+                                              "school": "XYZ High School",
+                                              "serialNumber": "T123",
+                                              "classRoom": "2A",
+                                              "students": []
+                                            }
+                                          ]
+                                        }
+                                    """)
+                    )
+                    ),
+                    @ApiResponse(
+                            description = "Not found student by SerialNumber: (serialNumber)",
+                            responseCode = "404",
+                            content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StudentDTO.class),
+                                    examples = @ExampleObject(value = "[]")
+                    )
+                    )
+            }
+    )
     @GetMapping(path = "/data/jpa/{serialNumber}")
     public ResponseEntity<StudentDTO> getStudentWithJpa(@PathVariable String serialNumber) {
         return studentService.getStudentBySerialNumberWithJpa(serialNumber);
